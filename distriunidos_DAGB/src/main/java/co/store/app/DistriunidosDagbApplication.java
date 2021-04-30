d3bb09255b908aa60cf0a557ef0651c19ee66bc0
Eskelgun <usbdaviid-098@hotmail.com>
2021-04-30 15:57:56 -0500

    1st push

---
+++ distriunidos_DAGB/src/main/java/co/store/app/DistriunidosDagbApplication.java
@@ -0,0 +1,13 @@
+package co.store.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistriunidosDagbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistriunidosDagbApplication.class, args);
+	}
+
+}
